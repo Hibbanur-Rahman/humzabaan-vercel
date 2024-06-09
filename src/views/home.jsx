a02d09d9bc708c50cc04a57372547ad50aed9b1f
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-06-09 18:21:25 +0530

    adding the ui of hero section

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,12 +1,23 @@
 import Navbar from "../components/navbar";
+import heroImg from "../assets/images/hero-img.png";
+import heroAnimation from "../assets/images/hero-animation.png";
+const Home = () => {
+  return (
+    <div className="row m-0 p-0 overflow-hidden">
+      {/** ================= Navbar-section ==================  */}
+      <Navbar />
 
-const Home=()=>{
-    return(
-        <div className="row m-0 p-0">
-            <Navbar/>
-            <h1>Home</h1>
+      {/** ================= Hero-section ==================  */}
+      <div className="hero-section row m-0 p-0 position-relative">
+        <img src={heroImg} alt="" className="m-0 p-0" />
+        <div className="hero-animation w-100  d-flex m-0 p-0  position-relative">
+          <img src={heroAnimation} alt="hero animation" className="m-0 p-0" />
+          <img src={heroAnimation} alt="hero animation" className="m-0 p-0" />
+          <img src={heroAnimation} alt="hero animation" className="m-0 p-0" />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
